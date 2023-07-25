f5f65036812e20220bd24c83fd1d4619007d65a4
Chaitanya Vaidya <chaitanya95vaidya@gmail.com>
2023-07-25 18:14:31 +0530

    #98 Loading navigation Properies #99 DataTable API

--- BulkyWeb/Areas/Admin/Controllers/ProductController .cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController .cs
@@ -20,7 +20,7 @@ namespace BulkyWeb.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
             
             return View(objProductList);
         }
@@ -155,5 +155,15 @@ namespace BulkyWeb.Areas.Admin.Controllers
             TempData["Success"] = "Category Deleted Successfully";
             return RedirectToAction("Index");
         }
+
+        #region
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new {data=objProductList});
+        }
+
+        #endregion
     }
 }
